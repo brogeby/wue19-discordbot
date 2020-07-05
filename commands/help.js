a3871cff3e31eefdc630002ac5654c31f46aeb4e
Brogeby <olof.brogeby@cmeducations.se>
2020-07-06 00:34:07 +0200

    fixing error with sorting

--- commands/help.js
+++ commands/help.js
@@ -13,7 +13,7 @@ module.exports = {
         
         if (!args.length) {
             data.push('Here\'s a list of all my commands:');
-            data.push(commands.map(command => command.name).join('\n').sort());
+            data.sort().push(commands.map(command => command.name).join('\n'));
             data.push(`\nYou can send \`${process.env.PREFIX}help [command name]\` to get info on a specific command!`);
             
             return message.author.send(data, { split: true })
