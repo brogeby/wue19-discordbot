243aa19eb0227a9ef981933385092f75ea771f52
Brogeby <olof.brogeby@cmeducations.se>
2020-07-06 12:27:29 +0200

    trying permissions

--- commands/king.js
+++ commands/king.js
@@ -1,16 +1,20 @@
 const { Client, MessageEmbed } = require('discord.js')
+let myRole = message.guild.roles.find(role => role.name === "admin");
 
 module.exports = {
     name: 'king',
     description: "Knugen",
     execute(message, args) {
-        if (!member.roles.cache.some(role => role.name === 'admin')) return
-        let text = args.join(" ")
-        const embed = new MessageEmbed()
-        .setTitle('KUNGEN')
-        .setColor(0xff0000)
-        .setDescription(text)
-        .setImage('https://www.kungahuset.se/images/200.44d65b65143d2fef3c9628/1390819079165/H.M._Konung_Carl_XVI_Gustaf_Kungl.Hovstaterna_Alexis_DaflosW.jpg')
-        message.channel.send(embed)
+        if(message.member.roles.has(myRole)) {
+            let text = args.join(" ")
+            const embed = new MessageEmbed()
+            .setTitle('KUNGEN')
+            .setColor(0xff0000)
+            .setDescription(text)
+            .setImage('https://www.kungahuset.se/images/200.44d65b65143d2fef3c9628/1390819079165/H.M._Konung_Carl_XVI_Gustaf_Kungl.Hovstaterna_Alexis_DaflosW.jpg')
+            message.channel.send(embed)
+        } else {
+            message.channel.send("You don't have permissions to use this command")
+        }
     }
 }
