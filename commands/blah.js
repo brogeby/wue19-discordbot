68c75e19e74d7462f4c3e08d09cc437efc51c59a
Brogeby <olof.brogeby@cmeducations.se>
2020-07-06 19:20:41 +0200

    going back without permissions

--- commands/blah.js
+++ commands/blah.js
@@ -1,10 +1,8 @@
-const { Client, Role, Guild } = require('discord.js')
-
 module.exports = {
     name: 'blah',
     description: "Blah blah blah",
     execute(message, args) {
-        if (!message.member.roles.find(r => r.name === "admin")) return message.channel.send("You don't have permission to use this command!")
-        message.channel.send('Meh.')
+        if (message.author.id !== '490100245917728770') return
+        message.channel.send('Meh.');
     }
 }
