a6bd59e324618580025e774bd74029ef1f31a0db
Brogeby <olof.brogeby@cmeducations.se>
2020-07-06 20:03:27 +0200

    added some errormessages

--- commands/announcement.js
+++ commands/announcement.js
@@ -4,12 +4,16 @@ module.exports = {
     name: 'announcement',
     description: "this checks the role ID!",
     execute(message, args) {
-        let text = args.join(" ")
-        const embed = new MessageEmbed()
-        .setTitle(':fire::fire::exclamation: ANNOUNCEMENT :exclamation::fire::fire:')
-        .setColor(0xff0000)
-        .setDescription(text)
-        message.delete()
-        message.channel.send(embed)
+        if (args.length > 0 ) {
+            let text = args.join(" ")
+            const embed = new MessageEmbed()
+            .setTitle(':fire::fire::exclamation: ANNOUNCEMENT :exclamation::fire::fire:')
+            .setColor(0xff0000)
+            .setDescription(text)
+            message.delete()
+            message.channel.send(embed)
+        } else {
+            message.channel.send('You have to type something! "!announcement Exampletext"')
+        }
     }
 }
--- commands/say.js
+++ commands/say.js
@@ -2,8 +2,12 @@ module.exports = {
     name: 'say',
     description: "This commands returns the value as a bot-message!",
     execute(message, args) {
-        let text = args.join(" ");
-        message.delete();
-        message.channel.send(text);
+        if (args.length > 0 ) {
+            let text = args.join(" ");
+            message.delete();
+            message.channel.send(text);
+        } else {
+            message.channel.send('You have to say something! "!say Exampletext"')
+        }
     }
 }
