476bc7eae72b649beb8897e8fab4b6297b592fe6
Brogeby <olof.brogeby@cmeducations.se>
2020-07-06 11:48:30 +0200

    minor styling to pirogen

--- commands/pirogen.js
+++ commands/pirogen.js
@@ -4,11 +4,9 @@ module.exports = {
     name: 'pirogen',
     description: "this checks the role ID!",
     execute(message, args) {
-        let text = args.join(" ")
         const embed = new MessageEmbed()
-        .setTitle('KUNGEN')
-        .setColor(0xff0000)
-        .setDescription(text)
+        .setTitle('PIROGEN')
+        .setColor(0x8B4513)
         .setImage('https://preview.redd.it/ylirml0nbog21.jpg?width=960&crop=smart&auto=webp&s=a02158ffc0d7d2c0983c5d18c1171d5d09815fe4')
         message.channel.send(embed)
     }
