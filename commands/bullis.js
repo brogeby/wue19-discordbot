9c339b85751992cfd7f845b0c4a3ab7ad21021b5
Brogeby <olof.brogeby@cmeducations.se>
2020-07-06 11:51:27 +0200

    added bullis

---
+++ commands/bullis.js
@@ -0,0 +1,13 @@
+const { Client, MessageEmbed } = require('discord.js')
+
+module.exports = {
+    name: 'bullis',
+    description: "this checks the role ID!",
+    execute(message, args) {
+        const embed = new MessageEmbed()
+        .setTitle('Bullis is to pure for this world :drool:')
+        .setColor(0x8B4513)
+        .setImage('https://ih1.redbubble.net/image.1191566311.5904/st,small,507x507-pad,600x600,f8f8f8.jpg')
+        message.channel.send(embed)
+    }
+}
