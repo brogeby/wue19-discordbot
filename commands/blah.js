27f57d44f8c0813fdfd5854585596bbb739b7576
Brogeby <olof.brogeby@cmeducations.se>
2020-07-06 19:13:02 +0200

    minor permission fixes

--- commands/blah.js
+++ commands/blah.js
@@ -2,11 +2,7 @@ module.exports = {
     name: 'blah',
     description: "Blah blah blah",
     execute(message, args) {
-        let modRole = message.guild.roles.find("name", "admin")
-        if (message.member.roles.has(modRole.id)) {
-            message.channel.send('Meh.')
-        } else {
-            message.channel.send("You don't have permission to request this command!")
-        }
+        if (!message.member.roles.find(r => r.name === "admin")) return message.channel.send("You don't have permission to use this command!")
+        message.channel.send('Meh.')
     }
 }
